34e77e9ada8fd6ed83a1f259d52528c35ec0f0c7
MervaHamo <119268778+MervaHamo@users.noreply.github.com>
2023-11-30 12:06:55 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+
 app.use(express.json());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
